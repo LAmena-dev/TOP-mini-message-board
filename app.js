d470b8d6e4cfbdbfd65df6471a1f5d85144d38c0
LAmena-dev <amena2laremm@gmail.com>
2026-03-05 12:41:05 +0800

    1.1.0 - styling done

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", indexRouter);
 
 const PORT = 3000;
